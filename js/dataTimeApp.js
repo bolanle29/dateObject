e5da198b43b8974f6f873dae227f01056ef12b37
Bolanle Ayodele <royaleunice29@gmail.com>
2021-09-13 18:52:25 +0100

    first commit

---
+++ js/dataTimeApp.js
@@ -0,0 +1,59 @@
+// Question 1
+       //To get age of User  
+// let year=eval(prompt("Enter your year of birth"))
+// console.log(dates.getFullYear())
+
+
+// dates=new Date()
+// let age = dates.getFullYear()-year
+// console.log("You are " + age + " years old")
+
+
+      
+
+       //Get Month
+// console.log(dates.getMonth()+1)
+      //Get Month
+// console.log(dates.getDate())
+      //    Get day
+// console.log(dates)
+      //Get Age
+// console.log(dates.getTime())
+
+
+
+
+// // Question 2
+
+// // dates=new Date
+// current = new Date()
+// // console.log(current)
+// expiry = new Date ("May 29 2023")
+// // dd=expiry.getFullYear()
+// let diff = expiry-current;
+// // console.log(diff)
+
+//       //  get days
+// let days = Math.floor(diff/(1000*60*60*24));
+
+//       // get hours
+// let hours = Math.floor(diff/(1000*60*60));   
+
+//       // get secs
+// let seconds = Math.floor(diff/(1000*60));   
+
+// if (expiry===current){
+//         console.log("Buhari's term end today" + expiry)
+//  }else{
+//        console.log("Buhari's Tenure ends in "+ days + " days, " + hours + " hour, " + seconds+ " seconds to go")
+//   }
+
+
+
+
+// Question 3
+
+current = new Date;
+// console.log(current);
+expiringDate = new Date;
+// console.log(expiringDate)
